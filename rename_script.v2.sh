545ec06fe197f5f138ae0d6f6356509cfa639ee6
Florin Irion <florin.irion@enterprisedb.com>
2022-01-20 10:52:18 +0100

    Remove prefix, adjust suffix
    The rename script is now only adding a double digit suffix.

--- rename_script.v2.sh
+++ rename_script.v2.sh
@@ -1,29 +1,31 @@
 #!/bin/sh
-# this script renames the files that start with digits addin a zero as prefix
-# and sequential suffix number after the original digit prefix found in the file name.
-# The suffix is increased only if we find the same original prefix otherwise it sets zero
-# it accepts the parameter "write" that decides if it will rename or only do a dry run
-# printing the original file name and what will it be renamed to.
+# this script renames the files that start with digits adding a
+# sequential two digit suffix number after the original digit prefix
+# found in the file name.  The suffix is increased only if we find the
+# same original prefix otherwise it sets zero (two digit) it accepts
+# the parameter "--write" that decides if it will actually rename or
+# only do a dry run printing the original file name and what will it
+# be renamed to.
 
-num=0
+num=00
 for file in $(ls [0-9]*pl); do
 
 	newprefix=$(echo $file | cut -f 1 -d '_')
 
 	if [[ $newprefix = $oldprefix ]]
 	then
-		let num=num+1
+		num=$(printf "%02d" ++num)
 	else
-		num=0
+		num=00
 	fi
 
 	rest=$(echo $file | cut -f 2- -d '_')
 
 	if [[ $1 = "--write" ]]
 	then
-		git mv "$file" "$(printf 0$newprefix$num)_$rest"
+		git mv "$file" "$(printf $newprefix$num)_$rest"
 	else
-		echo  "$file" "-->" "$(printf 0$newprefix$num)_$rest"
+		echo  "$file" "-->" "$(printf $newprefix$num)_$rest"
 	fi
 
 
